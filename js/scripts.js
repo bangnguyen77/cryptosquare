387ba35bd3b54fc5de443c1695d036cc5f80a339
Bang Nguyen and Leah Nelson <leah@>
2016-06-09 16:36:42 -0700

    split string into groups of five

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,32 @@
 function convert(originalSentence) {
   var sentenceArray = originalSentence.toLowerCase().split(" ").join("").split("");
   var columnNum = Math.ceil(Math.sqrt(sentenceArray.length));
+  var encryptedString= "";
 
-  var arrays = [], size = columnNum;
+  var totalArray = [], size = columnNum;
   while (sentenceArray.length > 0)
-      arrays.push(sentenceArray.splice(0, size));
-  console.log(arrays);
-};
+    totalArray.push(sentenceArray.splice(0, size));
 
-
-
-
-
-
-
-
-
+    for (var i=0; i<columnNum; i++) {
+      for (var k=0; k<totalArray.length; k++) {
+        encryptedString += totalArray[k][i];
+      }
+    }
+    var noUndefined = encryptedString.replace(/undefined/g, '');
+    var chunks = [];
+    var chunkSize = 5;
+    while(noUndefined) {
+        if (noUndefined.length < chunkSize) {
+            chunks.push(noUndefined);
+            break;
+        }
+        else {
+            chunks.push(noUndefined.substr(0, chunkSize));
+            noUndefined = noUndefined.substr(chunkSize);
+        }
+    }
+    return chunks;
+}
 
 $(document).ready(function(){
   var originalSentence = prompt("Enter a sentence:");
